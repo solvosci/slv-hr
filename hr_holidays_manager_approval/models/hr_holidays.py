ce467dab3db98e4b7a3cef37bbe73532fe1c4e56
David Alonso // Solvos <david.alonso@solvos.es>
2022-10-10 18:45:07 +0200

    [ADD] hr_holidays_manager_approval

---
+++ hr_holidays_manager_approval/models/hr_holidays.py
@@ -0,0 +1,55 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class HrHolidays(models.Model):
+    _inherit = "hr.holidays"
+
+    manager_user_id = fields.Many2one(
+        comodel_name="res.users",
+        compute="_compute_manager_user_id",
+        string="Manager",
+        store=True,
+        help="""
+        User that is allowed to approve/validate/refuse employee's leaves
+        """,
+    )
+
+    @api.multi
+    @api.depends("employee_id", "employee_id.parent_id")
+    def _compute_manager_user_id(self):
+        for holiday in self:
+            holiday.manager_user_id = (
+                holiday.employee_id.parent_id
+                and holiday.employee_id.parent_id.user_id
+                or holiday.employee_id.user_id
+            )
+
+    @api.multi
+    def action_approve(self):
+        for holiday in self:
+            holiday._check_security_manager_rights()
+        super(HrHolidays, self).action_approve()
+
+    @api.multi
+    def action_validate(self):
+        for holiday in self:
+            holiday._check_security_manager_rights()
+        super(HrHolidays, self).action_validate()
+
+    @api.multi
+    def action_refuse(self):
+        for holiday in self:
+            holiday._check_security_manager_rights()
+        super(HrHolidays, self).action_refuse()
+
+    @api.one
+    def _check_security_manager_rights(self):
+        if self.manager_user_id.id != self.env.user.id:
+            raise UserError(
+                _("Only %s can approve/validate/refuse leaves for the employee %s")
+                % (self.manager_user_id.name, self.employee_id.name)
+            )
