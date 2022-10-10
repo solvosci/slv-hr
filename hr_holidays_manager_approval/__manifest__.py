4355889ce5a5ce608210fecd83d31503576b03cd
David Alonso // Solvos <david.alonso@solvos.es>
2022-10-10 18:45:07 +0200

    [ADD] hr_holidays_manager_approval

---
+++ hr_holidays_manager_approval/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Employee Leaves - Manager approval",
+    "summary": """
+    Only user that is the Employee Manager of a certain employee can approve,
+    validate or refuese that employee leaves
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "11.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/solvosci/slv-hr",
+    "depends": ["hr_holidays"],
+    "data": ["views/hr_holidays_views.xml"],
+    "installable": True,
+}
---
+++ hr_holidays_manager_approval/readme/ROADMAP.rst
@@ -0,0 +1,2 @@
+* Disable/hide approve/validate/refuse buttons when the user can't actually
+  execute such operation.
