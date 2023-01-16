3ecbdc4349cc965d048cb6f56910ae8b735aabd2
LuPinheiro <gerallupinheiro@gmail.com>
2023-01-16 18:03:44 +0100

    [ADD] hr_attendance_user_edit_check_in_out

---
+++ hr_attendance_user_edit_check_in_out/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Hr Attendance User Edit check in out",
+    "summary": """
+        Enables to manual attendance users to later edit their own attendances
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Human Resources/Attendances",
+    "website": "https://github.com/solvosci/slv-hr",
+    "depends": [
+        "hr_attendance"
+    ],
+    "data": [        
+        "views/hr_attendance.xml"
+    ],
+    'installable': True,
+}
