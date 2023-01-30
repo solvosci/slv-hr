bb5da8c4bff1010b369e654677e91d5e566eed94
LuPinheiro <gerallupinheiro@gmail.com>
2023-01-30 17:49:14 +0100

    [ADD] hr_attendance_menu_filter

---
+++ hr_attendance_menu_filter/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Hr Attendance Menu Filter",
+    "summary": """
+        Hr Attendance Menu Filter
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Operations/HR",
+    "website": "https://github.com/solvosci/slv-hr",
+    "depends": [
+        "hr_attendance"
+    ],
+    "data": [   
+        "report/hr_attendance_report_view.xml",
+        "views/hr_attendance_view.xml",
+    ],
+    'installable': True,
+}
