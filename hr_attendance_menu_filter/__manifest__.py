dada2ec88364777af67d67e4a736cacc72d3c053
Ángel Rivas <angel.rivas@solvos.es>
2024-10-14 17:41:04 +0200

    [MIG] hr_attendance_menu_filter: Migration to 17.0
    Co-Authored-By: David Alonso <david.alonso@solvos.es>

--- hr_attendance_menu_filter/__manifest__.py
+++ hr_attendance_menu_filter/__manifest__.py
@@ -7,15 +7,15 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Operations/HR",
     "website": "https://github.com/solvosci/slv-hr",
     "depends": [
         "hr_attendance"
     ],
     "data": [   
-        "report/hr_attendance_report_view.xml",
         "views/hr_attendance_view.xml",
     ],
+    "uninstall_hook": "uninstall_hook",
     'installable': True,
 }
