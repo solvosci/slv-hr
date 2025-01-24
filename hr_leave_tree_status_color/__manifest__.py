e873bd076de9aad18d3b7aa89945d6cfe185d056
Ángel Rivas <angel.rivas@solvos.es>
2025-01-24 12:33:32 +0100

    [ADD] hr_leave_tree_status_color Adds colors in tree view to the leave status

---
+++ hr_leave_tree_status_color/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+{
+    "name": "Hr Leave Tree Status Color",
+    "summary": """
+        Adds colors in tree view to the leave status
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/solvosci/slv-hr",
+    "depends": ['hr_holidays'],
+    "data": [
+        "views/hr_leave_views.xml",
+    ],
+    'installable': True,
+}
