fbaf37e08933984bfc0216ccdb43237ffb643fdd
Iria Alonso <iria.alonso@solvos.es>
2025-01-30 11:52:33 +0100

    [ADD] hr_contract_tax_total New field representing the tax total percentaje

---
+++ hr_contract_tax_total/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Hr Contract Tax Total",
+    "summary": """
+        New field representing the tax total percentage
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/solvosci/slv-hr",
+    "depends": ["hr_contract"],
+    "data": ["views/hr_contract_views.xml"],
+    "installable": True,
+}
---
+++ hr_contract_tax_total/models/hr_contract.py
@@ -0,0 +1,19 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+
+from odoo import _,api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class HrContract(models.Model):
+    _inherit = "hr.contract"
+
+    tax_total_pct = fields.Float(
+        string="Personal Income Tax"
+    )
+
+    @api.constrains('tax_total_pct')
+    def _check_tax_total_pct(self):
+        for employee in self:
+            if not (0 <= employee.tax_total_pct <= 1):
+                raise ValidationError(_("The tax must be between 0 and 100. The tax of %s is %s%%") % (employee.employee_id.name, employee.tax_total_pct * 100))
