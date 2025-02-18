1c752417b9e7e5c93568e6a42d9eabe073e9c119
Iria Alonso <iria.alonso@solvos.es>
2025-02-18 11:28:51 +0100

    [ADD] hr_leave_has_attachment Adds new field to hr.leave model to check if there are any attachments.

---
+++ hr_leave_has_attachment/hooks.py
@@ -0,0 +1,21 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def pre_init_hook(cr):
+    logger.info("Creating field has_attachment on hr_leave")
+    cr.execute("""
+        ALTER TABLE hr_leave ADD COLUMN IF NOT EXISTS has_attachment boolean;
+    """)
+
+    logger.info("Computing field has_attachment on hr.leave")
+    cr.execute("""
+        UPDATE hr_leave SET has_attachment = TRUE
+        WHERE id IN (
+            SELECT res_id FROM ir_attachment 
+            WHERE res_model = 'hr.leave'
+        );
+    """)
