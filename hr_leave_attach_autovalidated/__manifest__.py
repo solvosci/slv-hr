3518ef30db0d2c1c327771402b8645ded8994845
Iria Alonso <iria.alonso@solvos.es>
2025-05-26 16:45:09 +0200

    [ADD] hr_leave_attach_autovalidated Allow to attach files on autovalidated absences.

---
+++ hr_leave_attach_autovalidated/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+{
+    "name": "Hr Leave Attach Autovalidated",
+    "summary": """
+        Allow to attach files on self-validated absences when they are validated.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/solvosci/slv-hr",
+    "depends": ['hr_holidays'],
+    "installable": True,
+}
