dc5588143a1b49532a0ce2b4fce9223bd455adc0
Iria Alonso <iria.alonso@solvos.es>
2025-12-16 10:55:31 +0100

    [ADD] hr_expense_hide_taxes Hide taxes field for expenses

---
+++ hr_expense_hide_taxes/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Hr Expense Hide Taxes",
+    "summary": """
+        Hide taxes field for expenses
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Operations/HR",
+    "website": "https://github.com/solvosci/slv-hr",
+    "depends": [
+        "hr_expense"
+    ],
+    "data": [
+        "views/hr_expense_views.xml",
+    ],
+    'installable': True,
+}
